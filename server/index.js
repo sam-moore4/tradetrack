e1d1bf80c37658b313d2ac39f7564cba6904a517
sam-moore98 <sam98.moore@hotmail.com>
2023-03-23 18:38:58 +1100

    Mongoose Set Up

--- server/index.js
+++ server/index.js
@@ -27,3 +27,17 @@ app.use("/trade", tradeRoutes); //Investments Held, Investment List, Trades(Tran
 app.use("/general", generalRoutes); //Users and Dashboard
 app.use("/history", historyRoutes); //Historical Transactions/ Daily/Monthly/Breakdown
 app.use("/management", managementRoutes); //Management Admin
+
+/* MONGOOSE SETUP */
+
+const PORT = process.env.PORT || 9000;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
